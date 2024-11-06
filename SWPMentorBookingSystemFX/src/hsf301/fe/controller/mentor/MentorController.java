d5b4b68b70b91923c2728e479d843e6a7501e5dd
LY THE VINH <vinhly123tn61@gmail.com>
2024-11-06 22:18:55 +0700

    profile mentor

--- SWPMentorBookingSystemFX/src/hsf301/fe/controller/mentor/MentorController.java
+++ SWPMentorBookingSystemFX/src/hsf301/fe/controller/mentor/MentorController.java
@@ -6,9 +6,12 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class MentorController {
 	@FXML
@@ -24,6 +27,9 @@ public class MentorController {
     private Button logoutButton;
 	
 	private Button selectedButton;
+	
+	private Stage stage;
+	private Scene scene;
 	
 	 @FXML
 	    private void initialize() {
@@ -41,30 +47,35 @@ public class MentorController {
         }
 	}
 	
-	@FXML
-	public void handleLoadBooking	(ActionEvent event) {
-		if (selectedButton != bookingButton) {
-            loadUI("Booking");
-            setSelectedButton(bookingButton);
-        }
-	}
+//	@FXML
+//	public void handleLoadBooking	(ActionEvent event) {
+//		if (selectedButton != bookingButton) {
+//            loadUI("Booking");
+//            setSelectedButton(bookingButton);
+//        }
+//	}
+//	
+//	@FXML
+//	public void handleTransactions(ActionEvent event) {
+//		if (selectedButton != transactionsButton) {
+//            loadUI("Transaction");
+//            setSelectedButton(transactionsButton);
+//        }
+//	}
 	
 	@FXML
-	public void handleTransactions(ActionEvent event) {
-		if (selectedButton != transactionsButton) {
-            loadUI("Transaction");
-            setSelectedButton(transactionsButton);
-        }
-	}
-	
-	@FXML
-	public void handleLogout(ActionEvent event) {
-		Platform.exit();
+	public void handleLogout(ActionEvent event) throws IOException {
+		 Parent loginScreen = FXMLLoader.load(getClass().getResource("/hsf301/fe/view/LoginUI.fxml"));
+	    	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	    	scene = new Scene(loginScreen);
+	    	stage.setScene(scene);
+	    	stage.centerOnScreen();
+	    	stage.show();
 	}
 	
 	private void loadUI (String uiName) {
 		try {
-			Parent screen = FXMLLoader.load(getClass().getResource("/hsf301/fe/view/customer/"+uiName+".fxml"));
+			Parent screen = FXMLLoader.load(getClass().getResource("/hsf301/fe/view/mentor/"+uiName+".fxml"));
 			contentPane.getChildren().clear();
 	        contentPane.getChildren().add(screen);
 		} catch (IOException e) {
